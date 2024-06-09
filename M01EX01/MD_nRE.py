a90458b37881c71b9c83cd4e6716a841030c86d7
Thái Lê Vân <82956224+thailevann@users.noreply.github.com>
2024-06-09 19:19:33 +0700

    Update MD_nRE.py

--- M01EX01/MD_nRE.py
+++ M01EX01/MD_nRE.py
@@ -1,13 +1,12 @@
-#caculate Mean Difference of nth Root Error:
+# Calculate Mean Difference of nth Root Error
 def MD_nRE(y_true, y_predict, n, p):
     return (y_true**(1/n) - y_predict**(1/n))**p
-if __name__== "__main__":
-    print("Input n")
-    n = input()
-    print("Input p")
-    p = input()
-    print("input y predict")
-    y_predict = input()
-    print("input y true")
-    y_true = input()
-    print(MD_nRE(float(y_true), float(y_predict), float(n), float(p)))
+
+if __name__ == "__main__":
+    n = float(input("Input n: "))
+    p = float(input("Input p: "))
+    y_predict = float(input("Input y predict: "))
+    y_true = float(input("Input y true: "))
+    
+    result = MD_nRE(y_true, y_predict, n, p)
+    print(result)
