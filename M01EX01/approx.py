9c89dff68c222a33b78252c5bd14508edf401331
Thái Lê Vân <82956224+thailevann@users.noreply.github.com>
2024-06-09 19:20:44 +0700

    Update approx.py

--- M01EX01/approx.py
+++ M01EX01/approx.py
@@ -1,69 +1,58 @@
-#Approximations for Sin(x), Cos(x), Sinh(x), Cosh(x)
+# Approximations for Sin(x), Cos(x), Sinh(x), Cosh(x)
 def factorial(x):
     fac = 1
-    if x  == 0 or x == 1:
-        return 1
-    for i in range(1, x+1):
-        fac = fac * i
+    for i in range(1, x + 1):
+        fac *= i
     return fac
 
 def approx_sin(x, n):
     approx = 0
-    for i in range(0,n):
-        s1 = (-1)**i
-        s2 = x**(2*i+1)
-        s3 = factorial(2*i+1)
-        s = s1*s2/s3
-        approx = approx + s
-    return approx
-def approx_cos(x,n):
-    approx = 0
-    for i in range(0,n):
-        s1 = (-1)**i
-        s2 = x**(2*i)
-        s3 = factorial(2*i)
-        s = s1*s2/s3
-        approx = approx + s
+    for i in range(n):
+        term = ((-1)**i) * (x**(2 * i + 1)) / factorial(2 * i + 1)
+        approx += term
     return approx
 
-def approx_sinh(x,n):
+def approx_cos(x, n):
     approx = 0
-    for i in range(0,n):
-        s2 = x**(2*i+1)
-        s3 = factorial(2*i+1)
-        s = s2/s3
-        approx = approx + s
+    for i in range(n):
+        term = ((-1)**i) * (x**(2 * i)) / factorial(2 * i)
+        approx += term
     return approx
 
-def approx_cosh(x,n):
+def approx_sinh(x, n):
     approx = 0
-    for i in range(0,n):
-        s2 = x**(2*i)
-        s3 = factorial(2*i)
-        s = s2/s3
-        approx = approx + s
+    for i in range(n):
+        term = (x**(2 * i + 1)) / factorial(2 * i + 1)
+        approx += term
     return approx
+
+def approx_cosh(x, n):
+    approx = 0
+    for i in range(n):
+        term = (x**(2 * i)) / factorial(2 * i)
+        approx += term
+    return approx
+
 def represents_int(s):
-    try: 
+    try:
         int(s)
+        return True
     except ValueError:
         return False
-    else:
-        return True
-if __name__== "__main__":
 
-    print("Input x = ")
-    x = input()
-    print("Input n = ")
-    n = input()
-    if represents_int(n) == True:
-        if n <= 0:
+if __name__ == "__main__":
+    x = input("Input x: ")
+    n = input("Input n: ")
+    
+    if represents_int(n):
+        n = int(n)
+        if n > 0:
             x = float(x)
-            n = int(n)
-            print(f'sin(x = {x}) = {approx_sin(x,n)}')
-            print(f'cos(x = {x}) = {approx_cos(x,n)}')
-            print(f'sinh(x = {x}) = {approx_sinh(x,n)}')
-            print(f'cosh(x = {x}) = {approx_cosh(x,n)}')
+            print(f'sin(x = {x}) ≈ {approx_sin(x, n)}')
+            print(f'cos(x = {x}) ≈ {approx_cos(x, n)}')
+            print(f'sinh(x = {x}) ≈ {approx_sinh(x, n)}')
+            print(f'cosh(x = {x}) ≈ {approx_cosh(x, n)}')
+        else:
+            print("n must be greater than zero")
     else:
-        print("n must be int and greater than zero")
-
+        print("n must be an integer and greater than zero")
